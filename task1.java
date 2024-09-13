958e4cd664456682d5171ebeb8bf7cfcfeb13bcd
aafil53 <mdaafil55@gmail.com>
2024-09-13 21:57:25 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,30 +1,23 @@
 //Java program to guess the number
 import java.util.Random;
 import java.util.Scanner;
-
 public class task1 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         final int MIN_NUMBER = 1;
         final int MAX_NUMBER = 100;
         final int MAX_ATTEMPTS = 10;
-
         boolean playAgain = true;
-
         while (playAgain) {
             int numberToGuess = random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
             int attempts = 0;
             boolean guessedCorrectly = false;
-
             System.out.println("Guess the number between " + MIN_NUMBER + " and " + MAX_NUMBER + ":");
-
             while (attempts < MAX_ATTEMPTS) {
                 System.out.print("Enter your guess: ");
                 int guess = scanner.nextInt();
                 attempts++;
-
                 if (guess == numberToGuess) {
                     System.out.println("Congratulations! You guessed the number " + numberToGuess + " in " + attempts + " attempts.");
                     guessedCorrectly = true;
@@ -35,16 +28,13 @@ public class task1 {
                     System.out.println("Too high!");
                 }
             }
-
             if (!guessedCorrectly) {
                 System.out.println("Sorry, you've used all " + MAX_ATTEMPTS + " attempts. The correct number was " + numberToGuess + ".");
             }
-
             System.out.print("Do you want to play another round? (yes/no): ");
             String response = scanner.next().trim().toLowerCase();
             playAgain = response.equals("yes");
         }
-
         System.out.println("Thanks for playing!");
         scanner.close();
     }
