2aa868925a9d953a17e5eb0bfbc7f673fa1525da
aafil53 <mdaafil55@gmail.com>
2024-09-13 22:21:39 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -5,9 +5,9 @@ public class task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        final int min_number = 1;
-        final int max_number = 100;
-        final int max_attempts = 10;
+        int min_number = 1;
+        int max_number = 100;
+        int max_attempts = 10;
         boolean playagain = true;
         while (playagain) {
             int numbertoguess = random.nextInt(max_number - min_number+1)+min_number;
