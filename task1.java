4db3489a244d37af8724d96c80bdafd76a6e6c17
aafil53 <mdaafil55@gmail.com>
2024-09-29 10:19:03 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,23 +1,36 @@
+//Java program to guess a number
+
 import java.util.Random;
 import java.util.Scanner;
+
 public class task1 {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int numberguess = random.nextInt(100) + 1; 
         int attempts = 10;
         System.out.println("Enter a number between 1 and 100.");
-        for (int i = 1; i <= attempts; i++) {
-            System.out.print(" Enter your guess:");
+
+        for (int i = 0; i < attempts; i++) {
+
+            System.out.print( " Enter your guess: ");
             int guess = sc.nextInt();
-            if (guess < numberguess) {
+
+            if (guess < numberguess) 
+            {
                 System.out.println("Very low!");
-            } else if (guess > numberguess) {
+            }
+            else if (guess > numberguess) 
+            {
                 System.out.println("Very high!");
-            } else {
+            }
+            else 
+            {
                 System.out.println("Congratulations! You guessed the number in " + i + " attempts!");
             }
         }
-        System.out.println("Sorry, you finished your attempts! The number is " + numberguess );
+        System.out.println("Sorry, you finished your attempts! The number is " + numberguess);
     }
 }
